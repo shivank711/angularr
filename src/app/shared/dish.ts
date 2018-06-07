d7da9a39f494e30f39abc5edfc4e53df2ab0ee80
shivank711 <imshivank09@gmail.com>
2018-06-07 15:33:47 -0700

    data binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,6 @@
+
+import {Comment} from './comment' 
+
 export class Dish{
 	name:string;
 	image: string;
@@ -5,5 +8,6 @@ export class Dish{
 	label:string;
 	price:string;
 	description:string;
+	comments : Comment[];
 }
 
